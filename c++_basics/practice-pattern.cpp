202b916ba27aeb1e6dce3d68edf1f3c811bbcbf7
ravi kumar <rk76912@gmail.com>
2021-09-10 15:32:10 +0530

    butterfly pattern

--- c++_basics/practice-pattern.cpp
+++ c++_basics/practice-pattern.cpp
@@ -4,19 +4,56 @@ using namespace std;
 
 int main(){
 
+// buttterfly pattern 
+int n;
+cout<<"Enter the number of row: ";
+cin>>n;
+
+for (int i=1; i<=n; i++){
+  for(int j = 1; j<=i; j++){
+    cout<<"*";
+    }
+    int space = 2*n-2*i;
+    for(int j=1; j<=space; j++){
+      cout<<" ";
+    }
+    for(int j = 1; j<=i; j++){
+    cout<<"*";
+  }
+  cout<<endl;
+
+}
+for (int i=n; i>=1; i--){
+  for(int j = 1; j<=i; j++){
+    cout<<"*";
+    }
+    int space = 2*n-2*i;
+    for(int j=1; j<=space; j++){
+      cout<<" ";
+    }
+    for(int j = 1; j<=i; j++){
+    cout<<"*";
+  }
+  cout<<endl;
+
+}
+
+
+
+
   //floyds triangle
 
-  int n,count=1;
-  cout<< "enter the no. of rows ";
-  cin>>n;
+  // int n,count=1;
+  // cout<< "enter the no. of rows ";
+  // cin>>n;
 
-  for(int i=1;i<=n;i++){
-    for(int j=1;j<=i;j++){
-      cout<<count <<" ";
-      count++;
-    }
-    cout<<endl;
-  }
+  // for(int i=1;i<=n;i++){
+  //   for(int j=1;j<=i;j++){
+  //     cout<<count <<" ";
+  //     count++;
+  //   }
+  //   cout<<endl;
+  // }
 
 
   // half pyramid using no. of rows

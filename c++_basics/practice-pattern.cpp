df309c3840741aaf2e4b0bb084ec3f7583de7399
ravi kumar <rk76912@gmail.com>
2021-09-10 14:31:54 +0530

    half inverted and normal half pyramid added

--- c++_basics/practice-pattern.cpp
+++ c++_basics/practice-pattern.cpp
@@ -4,11 +4,41 @@ using namespace std;
 
 int main(){
 
- int row,col; 
-cout<<"enter the no. of rows: ";
-cin>>row;
-cout<<"enter the no. of columns: ";
-cin>>col;
+//  int row,col; 
+// cout<<"enter the no. of rows: ";
+// cin>>row;
+// cout<<"enter the no. of columns: ";
+// cin>>col;
+
+// half pyramid 
+
+// int n;
+// cout<<"enter the no. of rows: ";
+// cin>>n;
+
+// for (int i=1;i<=n;i++){
+//   for(int j=1;j<=n;j++){
+//     if (j<=n-i){
+//       cout<<" ";
+//     }
+//     else{
+//       cout<<"*";
+//     }
+//     cout<<endl;
+// }
+
+
+
+// inverted half pyramid 
+int n;
+cin>>n;
+for (int i=n;i>=1;i--){
+  for(int j=1;j<=i;j++){ 
+    cout<<"*";
+  }
+  cout<<endl;
+
+}
 
 
 

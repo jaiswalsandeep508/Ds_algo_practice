1197ae1b4e24f5e812c3bab98796b943af119c89
ravi kumar <rk76912@gmail.com>
2021-09-10 14:06:16 +0530

    hollow and reactnge pattern added

---
+++ c++_basics/practice-pattern.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+ int row,col; 
+cout<<"enter the no. of rows: ";
+cin>>row;
+cout<<"enter the no. of columns: ";
+cin>>col;
+
+
+
+
+
+
+// hollow reactangle pattern 
+for (int i=1;i<=row;i++){
+  for (int j=1;j<=col;j++){
+    if (i==1 || i==row || j==1 || j==col){
+      cout<<"*";
+      }
+      else{
+        cout<<" ";
+      }
+  }
+  cout<<endl;
+}
+
+
+// rectange patter 
+// for (int i=1;i<=row;i++){
+//   for(int j=1;j<=col;j++){
+//     cout<<"*";
+//     }
+//     cout<<endl;
+// }
+  
+
+  return 0;
+
+}

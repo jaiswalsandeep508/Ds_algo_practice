71b74f7d0734d01c57de59f860dd90eb6e0db7fe
ravi kumar <rk76912@gmail.com>
2021-09-19 01:32:54 +0530

    inbuilt sort in descending order

--- c++_basics/sorting/inbulit_sort.cpp
+++ c++_basics/sorting/inbulit_sort.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 
 
+bool compare(int a ,int b){
+  return a>b;
+}
 
 int main(){
 
@@ -15,7 +18,7 @@ for (int i=0;i<n;i++){
   cin>>arr[i];
 }
 
-sort(arr,arr+n);
+sort(arr,arr+n,compare);
 
 for (int i=0;i<n;i++){
   cout<<arr[i]<<",";
